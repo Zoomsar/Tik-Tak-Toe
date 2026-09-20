04ee576302e2dbbafed8bb0ff06b44e8ae3fd50e
Ramses <rasmus.allmere@gmail.com>
2026-09-20 19:34:08 +0300

    feat: add game-board and indicate when game over in singleplayer lollololol

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
@@ -6,13 +6,10 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [isGameOver, setIsGameOver] =useState(false);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [letterPattern, setLetterPattern] = useState([]);
 
-  // Buttons will have letter id-s:
-  // A B C
-  // D E F
-  // G H I
-  const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+  const [letters, setLetters] = useState(["A", "B", "C", "D", "E", "F", "G", "H", "I"])
   const winningPatterns = [
     ["A", "B", "C"],
     ["D", "E", "F"],
@@ -24,16 +21,44 @@ function App() {
     ["C", "E", "G"]
   ]
 
+  function containsWinningPattern(currentLettersArray, winningPattern) {
+    return winningPattern.some(array => array.every(letter => currentLettersArray.includes(letter)))
+  }
+
+  function handleClick(letter) {
+    if (letterPattern.includes(letter)) {
+      return
+    }
+
+    setLetterPattern(prevPattern => [...prevPattern, letter])
+  }
+
+  useEffect(() => {
+      console.log(letterPattern)
+
+      if (containsWinningPattern(letterPattern, winningPatterns)) {
+        console.log("Game over!")
+        // temp
+        setLetters(prevLetters => prevLetters.map(letter =>
+          letter = ":)"
+        ))
+
+      }
+  }, [letterPattern])
+
   return (
     <>
       <h1>Tik Tak Toe</h1>
       <p>Next move: O/X</p>
 
-      <div class="game-board">
-        <button>Map 9 buttons</button>
+      <div className="game-board">
+        {/*<button>Map 9 buttons</button>*/}
+        {letters.map(letter => <button className="butts" id={letter} onClick={() => handleClick(letter)}>{letter}</button>)}
       </div>
 
-      <button>Reset Game</button>
+      <div className="reset">
+        <button>Reset Game</button>
+      </div>
     </>
   )
 }
